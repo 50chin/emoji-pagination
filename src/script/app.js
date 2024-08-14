3e40d834f1b23a5d8130f120a36ac93df6ffc426
50chin <50chin50@gmail.com>
2024-08-15 02:13:27 +0300

    feat js code

--- src/script/app.js
+++ src/script/app.js
@@ -1,27 +1,32 @@
-import { data } from "./emoji.js";
-const cardsNode = document.querySelector(".cards__wrapper");
-const inputNode = document.querySelector(".header__input");
+import { data } from './emoji.js';
+const cardsNode = document.querySelector('.cards__wrapper');
+const inputNode = document.querySelector('.header__input');
 
 function searchEmoji(evt) {
   const search = evt.target.value.toLowerCase().trim();
-  const newData = data.filter(
-    (el) =>
-      el.title.toLowerCase().includes(search) || el.keywords.includes(search)
-  );
-  renderCards(newData);
+  if (search.length === 0) {
+    return;
+  } else {
+    const newData = data.filter(
+      (el) =>
+        el.title.toLowerCase().includes(search) || el.keywords.includes(search)
+    );
+    renderCards(newData);
+    renderPagination();
+  }
 }
 
-inputNode.addEventListener("input", searchEmoji);
+inputNode.addEventListener('input', searchEmoji);
 
 // Функция по фильтрацию слов
 function filterWords(str) {
-  const filteredWords = [...new Set(str.split(" "))];
-  return filteredWords.join(" ");
+  const filteredWords = [...new Set(str.split(' '))];
+  return filteredWords.join(' ');
 }
 
 function createCard({ title, symbol, keywords }) {
-  const card = document.createElement("article");
-  card.className = "card";
+  const card = document.createElement('article');
+  card.className = 'card';
   card.innerHTML = `
     <p class='card__symbol'>${symbol}</p>
     <h3 class="card__title">${title}</h3> 
@@ -30,95 +35,106 @@ function createCard({ title, symbol, keywords }) {
   return card;
 }
 
-// const renderCards = (arr) => {
-//   cardsNode.innerHTML = "";
-//   arr.forEach((el) => {
-//     const card = createCard(el);
-//     cardsNode.append(card);
-//   });
-// };
+let start = 1;
+let end = 5;
 
-// renderCards(data);
-
-const selectNode = document.querySelector(".footer__select");
+const selectNode = document.querySelector('.footer__select');
 // Слушатель событий на select
-selectNode.addEventListener("change", () => {
-  changeSelect(), renderCards(data);
+selectNode.addEventListener('change', () => {
+  changeSelect(), renderCards(data), renderPagination(), (inputNode.value = '');
 });
 
 let defaultNum = 6;
+
 // Функция по выбору select
 function changeSelect() {
   defaultNum = selectNode.value;
   return defaultNum;
 }
 
-const footerListNode = document.querySelector(".footer__list");
-
-let end = 5;
-let start = 1;
+const footerListNode = document.querySelector('.footer__list');
 
 renderPagination(start, end);
 
 // Шаблон для Пагинации
-function renderPagination(start, end, num = 1) {
-  footerListNode.innerHTML = "";
+function renderPagination(start = 1, end = 5, num = 1, totalPage) {
+  footerListNode.innerHTML = '';
   footerListNode.innerHTML = `
  <li ><button class="footer__list-item" style=${
-   start === 1 ? "color:#ABB5BE;" : ""
- } type=${start === 1 ? "disabled" : ""}>First</button></li>`;
+   num === 1 ? 'color:#ABB5BE;' : ''
+ } type=${num === 1 ? 'disabled' : ''}>First</button></li>`;
   for (let i = start; i <= end; i++) {
     footerListNode.innerHTML += `<li><button class="footer__list-item"
-     style='${num === i ? "background:#055160; color:#FFF;" : ""}'
-    ${num === i ? "disabled" : ""}
+     style='${num === i ? 'background:#055160; color:#FFF;' : ''}'
+    ${num === i ? 'disabled' : ''}
     >${i}</button></li>`;
   }
   footerListNode.innerHTML += `<li ><button class="footer__list-item" style=${
-    end === 100 ? "color:#ABB5BE;" : ""
-  }  type=${end === 100 ? "disabled" : ""}>Last</button></li>`;
+    end === totalPage ? 'color:#ABB5BE;' : ''
+  }  type=${end === totalPage ? 'disabled' : ''}>Last</button></li>`;
 }
 
-
-
-footerListNode.addEventListener("click", (evt) => {
+footerListNode.addEventListener('click', (evt) => {
+  inputNode.value = '';
+  const choiceSelect = changeSelect();
+  let totalPage = Math.ceil(data.length / choiceSelect);
   let result = evt.target.innerText;
   let num;
-  if (result === "First") {
+  if (result === 'First') {
+    num = 1;
     start = 1;
     end = start + 4;
-  } else if (result === "Last") {
-    end = 100;
+  } else if (result === 'Last') {
+    end = totalPage;
     start = end - 4;
+    num = totalPage;
   } else {
     num = +result;
-    if (num === 1) {
+    if (num === 1 || num === 2 || num === 3) {
       start = 1;
-      end = 5;
-    } else if (num >= end - 2) {
-      start += 2;
-      end += 2;
-    } else if (num <= end - 2) {
-      start -= 2;
-      end -= 2;
+      end = start + 4;
+    } else if (
+      num === totalPage ||
+      num === totalPage - 1 ||
+      num === totalPage - 2
+    ) {
+      start = totalPage - 4;
+      end = totalPage;
+    } else if (num >= end - 1) {
+      start = num - 2;
+      end = num + 2;
+    } else if (num <= end - 1) {
+      start = num - 2;
+      end = num + 2;
     } else {
       start = start;
       end = end;
     }
   }
-  renderPagination(start, end, num);
+  renderPagination(start, end, num, totalPage);
+  renderCards(data, num);
 });
 
 // Функция по рендеру карточек
 
-function renderCards(data) {
-  let numPagination = 1;
+function renderCards(data, num = 1) {
   const selectNum = changeSelect();
-  numPagination *= selectNum;
-  cardsNode.innerHTML = "";
-  for (let i = 0; i < numPagination; i++) {
-    let indexData = data[i];
-    const card = createCard(indexData);
-    cardsNode.append(card);
+  let numPagination = num * selectNum;
+  let starPagination = (num - 1) * selectNum;
+  if (numPagination - data.length < 0) {
+    numPagination = numPagination;
+  } else {
+    numPagination = data.length;
+  }
+  if (data.length === 0) {
+    cardsNode.innerHTML = '<h2> ничего не найдено =(</h2>';
+  } else {
+    cardsNode.innerHTML = '';
+    for (let i = starPagination; i < numPagination; i++) {
+      let indexData = data[i];
+      const card = createCard(indexData);
+      cardsNode.append(card);
+    }
   }
 }
 
